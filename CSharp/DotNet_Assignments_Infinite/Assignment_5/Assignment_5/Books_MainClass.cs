02b5ff93b1e759dd06bc4416ff79390dff4def21
Rajesh Kesana <rajeshke@infinite.com>
2024-11-14 16:25:34 +0530

    Assignment-5

---
+++ CSharp/DotNet_Assignments_Infinite/Assignment_5/Assignment_5/Books_MainClass.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//1.Create a class called Books with BookName and AuthorName as members.
+//Instantiate the class through constructor and also write a method Display() to display the details. 
+//Create an Indexer of Books Object to store 5 books in a class called BookShelf.
+//Using the indexer method assign values to the books and display the same.
+
+//Hint(use Aggregation/composition)
+
+
+namespace Assignment_5
+{
+    public class Books
+    {
+        public string bookName { get; set; }
+        public string authorName { get; set; }
+
+        public Books(string bookName,string authorName)
+        {
+            this.bookName = bookName;
+            this.authorName = authorName;
+        }
+
+        public  void display()
+        {
+            Console.WriteLine($"Bookname is {bookName},written by {authorName} ");
+        }
+    }
+    public class BookShelf
+    {
+        private Books[] book = new Books[5];
+        public Books this[int i]
+        {
+            get { return book[i]; }
+            set { book[i] = value; }
+        }
+    }
+    class Books_MainClass
+    {
+        static void Main(string[] args)
+        {
+            BookShelf bookShelf = new BookShelf();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Enter The Deatils of Books");
+                Console.Write($"Enter Book {i + 1} BookName: ");
+                Console.WriteLine();
+                string bookName = Console.ReadLine();
+                Console.Write($"Enter Book {i + 1} AuthorName: ");
+                Console.WriteLine();
+                string authorName = Console.ReadLine();
+                bookShelf[i] = new Books(bookName,authorName);
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Book {i + 1} Details are");
+                bookShelf[i].display();
+            }
+            Console.ReadLine();
+        }
+       
+    }
+}
+
---
+++ CSharp/DotNet_Assignments_Infinite/Assignment_5/Assignment_5/Count_No_Of_Lines_In_The_File.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+//3.Write a program in C# Sharp to count the number of lines in a file.
+
+namespace Assignment_5
+{
+    class Count_No_Of_Lines_In_The_File
+    {
+        public static void Main(string[]args)
+        {
+            string path = @"C:\DotNet_Training\Infinite_1\Basics.txt";
+            if (File.Exists(path))
+            {
+                int count = File.ReadAllLines(path).Length;
+                Console.WriteLine("Number of Lines in the file is: {0}", count);
+            }
+            else
+            {
+                Console.WriteLine("File not Exist");
+            }
+            Console.Read();
+        }
+    }
+}
